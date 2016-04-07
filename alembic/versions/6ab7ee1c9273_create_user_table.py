a9a6fe4fb368716295c589b584291f91fff25050
jean <jean@bst>
2016-04-07 11:38:23 +0800

    close integrate migrations with alembic

---
+++ alembic/versions/6ab7ee1c9273_create_user_table.py
@@ -0,0 +1,30 @@
+"""create user table
+
+Revision ID: 6ab7ee1c9273
+Revises: 
+Create Date: 2016-04-07 01:18:55.804328
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6ab7ee1c9273'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(' \
+        CREATE TABLE users ( \
+            id INTEGER PRIMARY KEY, \
+            name VARCHAR(255), \
+            application VARCHAR(255), \
+            date TIMESTAMP \
+        )' \
+    )
+
+def downgrade():
+    conn = op.get_bind()
+    conn.execute('DROP TABLE users')
