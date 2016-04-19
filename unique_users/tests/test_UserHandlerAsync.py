5c6061c61b33b0696f7849d85dc2bea7e7029b51
jean <jean@bst>
2016-04-19 10:28:52 +0800

    close replace data= by json= and delete one useless function call

--- unique_users/tests/test_UserHandlerAsync.py
+++ unique_users/tests/test_UserHandlerAsync.py
@@ -6,5 +6,5 @@ BASE_URL = 'http://localhost:8080'
 
 def test_post_users():
     payload = {'name': 'Jean', 'application': 'app of jean'}
-    r = requests.post(BASE_URL + '/user', data=json.dumps(payload))
+    r = requests.post(BASE_URL + '/user', json=payload)
     assert r.status_code == 201
